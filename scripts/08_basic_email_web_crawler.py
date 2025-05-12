3f875d951322bc7156b4dc9d423defb57d80cc46
sinyaam <novagg43@gmail.com>
2025-05-12 14:53:14 +0300

    Add files via upload

--- scripts/08_basic_email_web_crawler.py
+++ scripts/08_basic_email_web_crawler.py
@@ -1,21 +1,21 @@
 import requests
-import re
+from bs4 import BeautifulSoup
+
 
 # get url
 url = input('Enter a URL (include `http://`): ')
 
 # connect to the url
-website = requests.get(url)
+responce = requests.get(url)
+html = responce.text
+soup = BeautifulSoup(html,"html.parser")
+print(html)
 
-# read html
-html = website.text
-
-# use re.findall to grab all the links
-links = re.findall('"((http|ftp)s?://.*?)"', html)
-emails = re.findall('([\w\.,]+@[\w\.,]+\.\w+)', html)
-
-
-# print the number of links in the list
-print("\nFound {} links".format(len(links)))
-for email in emails:
-    print(email)
+links = []
+for i in soup.find_all("a",href=True):
+    links.append(i)
+    print("leitud link: ", i)
+# # print the number of links in the list
+# print("\nFound {} links".format(len(links)))
+# for email in emails:
+#     print(email)
