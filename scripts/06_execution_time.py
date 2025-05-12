7d3dcd1437c649e6729f31825308520c185eb75a
sinyaam <novagg43@gmail.com>
2025-05-12 14:13:12 +0300

    Add files via upload

--- scripts/06_execution_time.py
+++ scripts/06_execution_time.py
@@ -1,34 +1,50 @@
-"""
-ExecutionTime
-
-This class is used for timing execution of code.
-
-For example:
-
-    timer = ExecutionTime()
-    print 'Hello world!'
-    print 'Finished in {} seconds.'.format(timer.duration())
-
-"""
-
-
+# """
+# ExecutionTime
+# 
+# This class is used for timing execution of code.
+# 
+# For example:
+# 
+#     timer = ExecutionTime()
+#     print 'Hello world!'
+#     print 'Finished in {} seconds.'.format(timer.duration())
+# 
+# """
+# 
+# 
 import time
 import random
-
+# 
+# 
+# class ExecutionTime:
+#     def __init__(self):
+#         self.start_time = time.time()
+# 
+#     def duration(self):
+#         return time.time() - self.start_time
+# 
+# 
+# # ---- run code ---- #
+# 
+# 
+# timer = ExecutionTime()
+# sample_list = list()
+# my_list = [random.randint(1, 888898) for num in
+#            range(1, 1000000) if num % 2 == 0]
+# print('Finished in {} seconds.'.format(timer.duration()))
 
 class ExecutionTime:
-    def __init__(self):
-        self.start_time = time.time()
-
-    def duration(self):
-        return time.time() - self.start_time
-
-
-# ---- run code ---- #
-
-
+      def __init__(self):
+          self.start_time = time.time()
+          
+      def duration(self):
+          return time.time() - self.start_time
+my_list = [] 
 timer = ExecutionTime()
-sample_list = list()
-my_list = [random.randint(1, 888898) for num in
-           range(1, 1000000) if num % 2 == 0]
-print('Finished in {} seconds.'.format(timer.duration()))
+for num in range(1, 1000000):
+    if num % 2 == 0:
+        my_list.append(random.randint(1, 888898))
+for i in range(1,10):
+    print("hi!!!")
+        
+print ('Finished in {} seconds.'.format(timer.duration()))
